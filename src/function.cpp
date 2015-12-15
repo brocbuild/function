260e13f3eaf5b2501d6764ae4c98da9f53aceb83
liruihao <liruihao@baidu.com>
2015-12-15 14:43:11 +0800

    add BROC and source files

---
+++ src/function.cpp
@@ -0,0 +1,49 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2015 Baidu.com, Inc. All Rights Reserved
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * @file ub.cpp
+ * @author liruihao(com@baidu.com)
+ * @date 2015/12/15 14:39:16
+ * @brief 
+ *  
+ **/
+
+#include "brocbuild/function/src/function.h"
+
+function::function()
+{
+}
+
+function::~function()
+{
+}
+
+bool function::is_function()
+{
+    return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
---
+++ src/function.h
@@ -0,0 +1,45 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2015 Baidu.com, Inc. All Rights Reserved
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * @file function.h
+ * @author liruihao(com@baidu.com)
+ * @date 2015/12/15 14:41:44
+ * @brief 
+ *  
+ **/
+
+
+
+
+#ifndef  __FUNCTION_H_
+#define  __FUNCTION_H_
+
+
+class function {
+    public:
+        function();
+        ~function();
+        bool is_function();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif  //__FUNCTION_H_
+
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
